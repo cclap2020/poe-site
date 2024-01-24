f7b4ad4781c58522c1940c91a3c1869043f15ee6
Chingche Lin <wchingchelin@gmail.com>
2024-01-23 23:37:23 -0500

    ci: failed build

--- next.config.js
+++ next.config.js
@@ -1,7 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: { unoptimized: true },
-  experimental: { images: { unoptimized: true } },
   basePath: "/poe-site",
   output: "export",
 };
